6f940fb5d68fdc8cf8ef3990d30f73a97ff95462
dogyeong <shroad1802@naver.com>
2021-01-23 16:53:37 +0900

    feat: style

--- src/js/view.js
+++ src/js/view.js
@@ -2,6 +2,8 @@ import Event from './event.js';
 
 const View = function ($target) {
   this.$target = $target;
+  this.$container;
+
   this.noteAddEvent = new Event();
   this.noteSaveEvent = new Event();
   this.noteDelEvent = new Event();
@@ -9,9 +11,21 @@ const View = function ($target) {
   this.cancelEvent = new Event();
 
   this.init();
+  this.on();
 };
 
 View.prototype.init = function () {
+  this.$target.innerHTML = `
+    <div class="app-container">
+      <h1>Notes App</h1> 
+      <div class="container"></div>
+    </div>
+  `;
+
+  this.$container = document.querySelector('.container');
+};
+
+View.prototype.on = function () {
   this.$target.addEventListener('click', ({ target }) => {
     if (target.classList.contains('add-btn')) {
       this.noteAddEvent.trigger();
@@ -35,37 +49,35 @@ View.prototype.init = function () {
 
 View.prototype.render = function (viewMode, notes) {
   if (viewMode === 0) {
-    this.$target.innerHTML = `
-    <div class="container">
-      <button type="button" class="add-btn">추가</button>
-      ${notes
-        .map(
-          (note) => `
-          <li><button class='detail' data-id=${note.id}>${note.title}</button></li>
-          `,
-        )
-        .join('')}
-    </div>
+    this.$container.innerHTML = `
+      <ul class="note-list">
+        ${notes
+          .map(
+            (note) => `
+            <li><button class='detail' data-id=${note.id}>${note.title}</button></li>
+            `,
+          )
+          .join('')}
+      </ul>
+      <button type="button" class="add-btn">새 노트 추가</button>
   `;
   } else if (viewMode === 1) {
     const id = notes;
-    this.$target.innerHTML = `
-      <div class="container">
-        <input type="text" class="note-title" />
-        <textarea class="note-text"></textarea>
-        <button class="save-btn" data-id=${id}>추가</button>
-        <button class="cancel-btn">취소</button>
-      </div>
+    this.$container.innerHTML = `
+      <input type="text" class="note-title" placeholder="제목" />
+      <textarea class="note-text" placeholder="내용" rows="5"></textarea>
+      <button class="save-btn" data-id=${id}>추가</button>
+      <button class="cancel-btn">취소</button>
     `;
   } else if (viewMode === 2) {
-    this.$target.innerHTML = `
-      <div class="container">
-        <button class="cancel-btn">뒤로가기</button>
-        <button class="edit-btn" data-id=${notes.id}>수정</button>
-        <button class="del-btn" data-id=${notes.id}>삭제</button>
+    this.$container.innerHTML = `
+      <div class="note-detail">
         <h3>${notes.title}</h3>
         <p>${notes.text}</p>
       </div>
+      <button class="cancel-btn">뒤로가기</button>
+      <button class="edit-btn" data-id=${notes.id}>수정</button>
+      <button class="del-btn" data-id=${notes.id}>삭제</button>
     `;
   }
 };
